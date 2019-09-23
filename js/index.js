9c6d42cf1d09848b7c5148b733aa057bd52aa298
labtopSky <2skyer@naver.com>
2019-09-23 15:31:34 +0900

    fix blank js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,17 @@
 (function (){
     
-    // mouseover&nonover title event
-    const title = document.querySelector('.title');
-    let titleTop = false;
-    
-    title.addEventListener('mouseover',function(){
-        if(!titleTop){
-            title.classList.add('titleHover');
-        }
-    })
-    title.addEventListener('mouseout', function(){
-        title.classList.remove('titleHover');
-    })
-    
     // click title event
+    const title = document.querySelector('.title');
+
     title.addEventListener('click', function(){
-        console.dir(title);
         title.style.transitionDuration = '0.5s';
         title.style.transform = 'translateY(-30vh)'
 
         setTimeout(function(){
             title.style.transitionDuration = '0s';
-            titleTop = true;
         }, 500);
     });
+    
     
     
 })();
